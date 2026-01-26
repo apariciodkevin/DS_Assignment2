c4ce0a17e858fb3a931c67b5c015b347147ee81b
KevnAp <apariciodkevin@Gmail.com>
2026-01-26 10:33:36 -0600

    started constructing questoins for the user

--- src/Aparicio_assignment2.java
+++ src/Aparicio_assignment2.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Aparicio_assignment2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Setting my public variables
+        double loanAmount;
+        float interestRate;
+        int years;
+        System.out.println("How much is the loan amount?");
+        loanAmount = sc.nextDouble();
+        System.out.println("What is the annual interest rate?");
+        interestRate = sc.nextFloat();
+        System.out.println("Enter the number of years: ");
+        years = sc.nextInt();
+
+
+
+
+
 
     }
 }
