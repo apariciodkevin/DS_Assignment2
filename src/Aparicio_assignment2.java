112c6efb142c2d434e7cb635c096e04d85f082c4
KevnAp <apariciodkevin@Gmail.com>
2026-01-26 23:11:46 -0600

    Added comments to file

--- src/Aparicio_assignment2.java
+++ src/Aparicio_assignment2.java
@@ -8,6 +8,7 @@ public class Aparicio_assignment2 {
         float interestRate;
         int years;
 
+        // Promp questions for user
         System.out.println("How much is the loan amount?");
         loanAmount = sc.nextDouble();
         System.out.println("What is the annual interest rate?");
@@ -15,19 +16,24 @@ public class Aparicio_assignment2 {
         System.out.println("Enter the number of years: ");
         years = sc.nextInt();
 
+        // print user input to console
         System.out.printf("Loan Amount: $%.2f", loanAmount);
         System.out.println();
+
         System.out.printf("Interest Rate: %.2f%%", interestRate * 100);
         System.out.println();
 
+        // calculate monthly payment
         double monthlyPayment = (interestRate * loanAmount) / (1 - Math.pow(1 + interestRate, -years * 12));
         System.out.printf("Monthly Payment: $%.2f", monthlyPayment);
         System.out.println();
 
+        // calculate total cost
         double totalCost = monthlyPayment * years * 12;
         System.out.printf("Total Cost: $%.2f", totalCost);
         System.out.println();
 
+        // calculate ratio over principal
         double ratioOverPrincipal = ((monthlyPayment * years * 12) - loanAmount) * 100 / loanAmount;
         System.out.printf("Ratio paid over principal: %.2f%%", ratioOverPrincipal);
         System.out.println();
