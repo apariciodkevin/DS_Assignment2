bfb902bc28f48cebe328244d28993f736fff9c9a
KevnAp <apariciodkevin@Gmail.com>
2026-01-28 12:04:25 -0600

    Fixed certain equations as well as added more functionality. Including the notification once a year cycle ends

--- src/Aparicio_assignment2.java
+++ src/Aparicio_assignment2.java
@@ -5,14 +5,14 @@ public class Aparicio_assignment2 {
         Scanner sc = new Scanner(System.in);
         // Setting my public variables
         double loanAmount;
-        float interestRate;
+        double interestRate;
         int years;
 
         // Promp questions for user
         System.out.println("How much is the loan amount?");
         loanAmount = sc.nextDouble();
         System.out.println("What is the annual interest rate?");
-        interestRate = sc.nextFloat();
+        interestRate = sc.nextDouble();
         System.out.println("Enter the number of years: ");
         years = sc.nextInt();
 
@@ -25,7 +25,7 @@ public class Aparicio_assignment2 {
 
         // calculate monthly payment
         int loanPeriod = years * 12;
-        double monthlyPayment = (interestRate * loanAmount) / (1 - Math.pow(1 + interestRate, -loanPeriod));
+        double monthlyPayment = ((interestRate/12) * loanAmount) / (1 - Math.pow(1 + (interestRate/12), -loanPeriod));
         System.out.printf("Monthly Payment: $%.2f", monthlyPayment);
         System.out.println();
 
@@ -39,8 +39,45 @@ public class Aparicio_assignment2 {
         System.out.printf("Ratio paid over principal: %.2f%%", ratioOverPrincipal);
         System.out.println();
 
+        // Calculating the balance
+        double balance = loanAmount;
 
+        for (int period = 1; period <= loanPeriod; period++){
+            double interestAmount = balance * (interestRate/12);
+            double principalAmount = monthlyPayment - interestAmount;
+            balance -= (monthlyPayment - interestAmount);
+
+            System.out.printf("Monthly Payment: $%.2f", monthlyPayment);
+            System.out.printf(" Principal Amount: $%.2f", principalAmount);
+            System.out.printf(" Interest Amount: $%.2f", interestAmount);
+            System.out.printf(" Amount Left: $%.2f", balance);
+
+            System.out.println();
+
+            // output each year in the statement
+            if (period % 12 == 0){
+                int year = period / 12;
+                System.out.println("--- End of Year " + year + "---");
+            }
+
+            if(monthlyPayment>=balance){
+                monthlyPayment = balance;
+                principalAmount = monthlyPayment;
+                interestAmount = 0;
+                balance -= monthlyPayment;
+
+                System.out.printf("Monthly Payment: $%.2f", monthlyPayment);
+                System.out.printf(" Principal Amount: $%.2f", principalAmount);
+                System.out.printf(" Interest Amount: $%.2f", interestAmount);
+                System.out.printf(" Amount Left: $%.2f", balance);
+
+                System.out.println();
+                System.out.println("--- End of Year " + ((period/12) + 1) + " ---");
+
+                // Once it reaches condition it stops from repeating calculation
+                break;
+            }
+
+        }
     }
-
-
 }
